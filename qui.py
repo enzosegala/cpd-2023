fd365f151aa1a5566b7ea20ebf33882d54f6af75
enzosegala <61666918+enzosegala@users.noreply.github.com>
2023-06-26 07:45:33 -0300

    Add files via upload

--- qui.py
+++ qui.py
@@ -1,5 +1,7 @@
 import numpy as np
 import time
+recur=0
+
 ########################## leitura ##########################
 array=[]
 with open('entrada-quicksort.txt', 'r') as f:
@@ -49,19 +51,40 @@ def funcao_mediana(ini,meio,fim):
 ####  A = vetor de entrada
 ####  p = indice menor elemento
 ####  r = indice maior elemento
-def partition_lomuto(A,p,r):
-    chave = A[p]
-    storeindex= p+1
-    i=p+1
-    for i in range(p,r+1):
-        if A[i]<chave: 
-            i+=1
-            swap(A[i],A[storeindex])
-            # swap(A[i],A[i+1])
-            storeindex+=1
-        
-    swap(A[p],A[storeindex-1])
-    return (storeindex -1 )
+# def partition_lomuto(A,p,r):
+#     global n_swaps
+#     chave = A[r]
+#     storeindex= p-1
+
+#     for i in range(p,r,1):
+#         if A[i]<chave: 
+            
+#             swap(A[i],A[storeindex])
+#             storeindex+=1 
+#             n_swaps+=1
+#             # swap(A[i],A[i+1]) 
+#     swap(A[r],A[storeindex+1])
+#     return(storeindex+1)
+
+def partition_lomuto(arr, low, high):
+     
+    # pivot
+    pivot = arr[high]
+     
+    # Index of smaller element
+    i = (low - 1)
+    for j in range(low, high):
+         
+        # If current element is smaller than or
+        # equal to pivot
+        if (arr[j] <= pivot):
+             
+            # increment index of smaller element
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return (i + 1)
+     
 
 def partition_hoare(A,p,r):
     chave =A[p]
@@ -76,10 +99,6 @@ def partition_hoare(A,p,r):
             A[j]=A[i]
     A[j]=chave
     return i 
-
-
-
-
 
 
 def partition(A,p,r):
@@ -97,6 +116,11 @@ def partition(A,p,r):
     # A[i+1]=A[r]
     # A[r]=temp2
     return i+1
+
+
+#####################chamada quicksort ##############
+
+
     ############################# random 
 def random_quicksort(A,p,r):
     if r > p:
@@ -114,6 +138,22 @@ def quicksort(A,p,r):
         q=partition(A,p,r)
         quicksort(A,p,q-1)        
         quicksort(A,q+1,r)
+
+def quicksort_lomuto(A,p,r):
+    if p<r: 
+        global recur 
+        recur+=1
+        print(r)
+        q=partition_lomuto(A,p,r)
+        quicksort_lomuto(A,p,q-1) 
+        quicksort_lomuto(A,q+1,r)
+        # return#,n_swaps+n_swaps2
+
+def quicksort_hoare(A,p,r):
+    if p <r:
+        q=partition_hoare(A,p,r)
+        quicksort_hoare(A,p,q-1)        
+        quicksort_hoare(A,q+1,r)                
 #################### mediana
 
 
@@ -121,21 +161,41 @@ def mediana_quicksort_hoare(A,p,r):
     if p<r:
         mediana=funcao_mediana(A[p],A[(len(A[1:])-1)//2],A[r])
         q=partition_hoare(A,p,mediana)
-        quicksort(A,p,q-1)        
-        quicksort(A,q+1,r)
+        quicksort_hoare(A,p,q-1)        
+        quicksort_hoare(A,q+1,r)
         
-st = time.time()
+
 def mediana_quicksort_lomuto(A,p,r):
     if p<r:
+        global recur
+        recur=0
+        global n_swaps
+        n_swaps=0
         mediana=funcao_mediana(A[p],A[(len(A[1:])-1)//2],A[r])
         q=partition_lomuto(A,p,mediana)
-        quicksort(A,p,q-1)        
-        quicksort(A,q+1,r)
-    
-mediana_quicksort_lomuto(array_novo[4],1,len(array_novo[4])-1)
-et = time.time()
-#########################
-# random_quicksort(array_novo[0],0,len(array_novo[0])-1)
+        
+        quicksort_lomuto(A,p,q-1)        
+        quicksort_lomuto(A,q+1,r)
+        
+        return A
+###########################3
 
-print(array_novo[0],"tempo=%f"%(et-st))
-# print(array_novo[2])
+print(mediana_quicksort_lomuto(array_novo[0],1,len(array_novo[0])-1))
+# with open('stats-mediana-lomuto.txt.txt', 'w') as f:
+#     for i in range(0,5):
+#         st = time.time()
+#         mediana_quicksort_lomuto(array_novo[i],1,len(array_novo[i])-1)
+#         et = time.time()
+#         f.write('TAMANHO ENTRADA %i'%(len(array_novo[i])-1))
+#         f.write('\n')
+
+#         f.write('SWAPS %i'%n_swaps)
+#         f.write('\n')
+
+#         f.write('RECURSOES %i' %(recur))
+#         f.write('\n')
+
+#         f.write('TEMPO %f'%(et-st))
+#         f.write('\n')
+
+
